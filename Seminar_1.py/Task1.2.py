94de75365bb92ec879ba7d3218a58b1d99d681ab
Olga <olgajensen@mail.ru>
2022-12-19 01:51:50 +0300

    Task1.2

--- Seminar_1.py/Task1.2.py
+++ Seminar_1.py/Task1.2.py
@@ -6,5 +6,5 @@ for w in range(2):
     for x in range(2):
         for y in range(2):
             for z in range(2):
-                if not (w and z or y or x == w):
+                if not (w and z or not y or not (x == w)):
                     print(w, x, y, z)

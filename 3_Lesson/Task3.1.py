e6c66cdcd2a4b8c0e8c49c075aafca2ee9d56ec5
Olga <olgajensen@mail.ru>
2022-12-22 23:56:07 +0300

    3.1

--- 3_Lesson/Task3.1.py
+++ 3_Lesson/Task3.1.py
@@ -4,9 +4,10 @@
 
 from random import sample
 
-
-
 def num_find(len_list, number):
     new_list = sample(range(1, len_list*2), k=len_list)
     print(new_list)
-num_find(int(input()), int(input()))    
+    if number in new_list:
+        return"Yes"
+    return"No"
+print(num_find(int (input("Введите длину списка: ")), int (input("Введите число:"))))  

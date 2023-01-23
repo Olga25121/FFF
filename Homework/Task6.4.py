da0f00c4c033a676fc00989a1ab375117922bf7a
Olga <olgajensen@mail.ru>
2023-01-23 11:07:05 +0300

    Функция принимает в качестве аргументов строки в формате «Имя Фамилия», возвращает словарь, ключи — первые буквы фамилий, значения — словари, реализованные по схеме предыдущего задания.

--- Homework/Task6.4.py
+++ Homework/Task6.4.py
@@ -8,6 +8,7 @@
 
 # from itertools import groupby
 
+#1
 def thesaurus_adv(*args):
     s_n_sort = {}
     for s_n in args:
@@ -18,3 +19,19 @@ def thesaurus_adv(*args):
 print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев",
                     "Илья Иванов", "Анна Савельева", "Юнона Ветрякова",
                     "Борис Аркадьев", "Антон Серов", "Павел Анисимов"))
+
+#2
+from collections import OrderedDict
+
+
+def thesaurus_adv(*args):
+    s_n_sort = {}
+    for s_n in args:
+        s_n_sort.setdefault(s_n.split()[1][0], {}).setdefault(s_n.split()[0][0], []).append(s_n)
+    return dict(OrderedDict(sorted(s_n_sort.items(), key=lambda x: x[0])))
+
+
+fio_name = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев",
+                         "Илья Иванов", "Анна Савельева", "Юнона Ветрякова",
+                         "Борис Аркадьев", "Антон Серов", "Павел Анисимов")
+print(fio_name)

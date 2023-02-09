460d67edc47fc57ea9c9075601e8c55b41d12f8e
Olga <olgajensen@mail.ru>
2023-02-09 20:20:43 +0300

    commit

--- 10_Lesson/X_O/loader.py
+++ 10_Lesson/X_O/loader.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from loguru import logger
 
-bot = Bot(BOT_TOKEN)
+bot = Bot("BOT_TOKEN")
 dp = Dispatcher(bot)  # обработчик
 
 logger.add('log_info.log',
